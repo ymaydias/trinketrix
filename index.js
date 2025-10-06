3c884621d9e8811639c3833cf07dbd7b98cfa6ae
Mayara Dias <127147013+ymaydias@users.noreply.github.com>
2025-10-05 23:02:28 -0300

    first

---
+++ index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAPent17Lo7YnB81UwBXGnw4-aWSQDgozM",
+  authDomain: "trinketrix-ad849.firebaseapp.com",
+  projectId: "trinketrix-ad849",
+  storageBucket: "trinketrix-ad849.firebasestorage.app",
+  messagingSenderId: "328519118113",
+  appId: "1:328519118113:web:c0fe4b2bd9b7d80159e260"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
